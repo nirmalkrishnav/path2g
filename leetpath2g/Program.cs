d59ed4b7bd64aeeb93672c85b1d0e843ca687de8
Nirmal Krishna <nirmalkrishnav@gmail.com>
2023-08-17 20:17:27 +0530

    adding git

---
+++ leetpath2g/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+// https://leetcode.com/problems/running-sum-of-1d-array/description/
+int[] nums = { 1, 2, 3, 4 };
+for (int i = 1; i < nums.Length; i++)
+{
+    nums[i] += nums[i - 1];
+}
+Console.WriteLine(nums);
